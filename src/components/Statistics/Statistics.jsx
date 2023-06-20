adff2cf7072a112b76a5c17879b3626b5077bc94
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-06-20 20:23:21 +0200

    finish add

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import css from './Statistics.module.css';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul className={css['feedback-item']}>
+      <li className={css.item}>
+        <p className={css.text}>Good: {good}</p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>Neutral: {neutral}</p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>Bad: {bad}</p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>Total: {total}</p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>Positive feedback: {positivePercentage}%</p>
+      </li>
+    </ul>
+  );
+};
+
+export default Statistics;
