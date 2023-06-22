be5a03e507d657a20d05262e7327ebe2061fe030
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-06-22 14:34:50 +0200

    finish add

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 import { Emojione } from 'react-emoji-render';
 
@@ -21,6 +22,11 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
       ))}
     </ul>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
